16ff31daaf83b6060909845bfc7a57bd0d3bf2c5
lissandrito <luisdeveloper2@gmail.com>
2025-05-03 17:17:43 -0700

    Fin dia 4
    Se termino el proyecto del dia 4

--- Codigo/Dia 4/proyecto Dia 4/script.js
+++ Codigo/Dia 4/proyecto Dia 4/script.js
@@ -1,6 +1,7 @@
 //creando las variables de las operaciones
 // Todo en javascript es un objeto
-
+let ultimoResultado = null; // guardar el resultado de manera global para que ambas funcines puedan utilizarla
+//Funciones principales
 function sumar(numero1, numero2) {
   let resultado_sum = +numero1 + +numero2;
   return resultado_sum;
@@ -29,23 +30,17 @@ function absoluto(numero2) {
   let resultado_abso = Math.abs(+numero2);
   return resultado_abso;
 }
-function absoluto(numero2) {
-  let resultado_abso = Math.abs(+numero2);
-  return resultado_abso;
-}
 function randomice(numero1, numero2) {
   numero1 = parseInt(numero1);
   numero2 = parseInt(numero2);
   let numeroAletorio =
     Math.floor(
       Math.random() *
-        (Math.max(numero1, numero2) - Math.min(numero1, numero2) + 1)
+        (Math.max(numero1, numero2) - Math.min(numero1, numero2) + 1) // utilizando min y max podemos hacer mas grande el rango de numeros
     ) + Math.min(numero1, numero2);
   return numeroAletorio;
 }
-
 // Creando la funcion cerebro
-
 function regresar(operacion) {
   // la operacion es el parametro que recibe al presionarse un boton con el evento de abajo
   let valor1 = document.getElementById("ingreso1");
@@ -71,19 +66,55 @@ function regresar(operacion) {
     resultado = randomice(valor1.value, valor2.value);
   }
 
+  ultimoResultado = resultado; // en cada operacion, el resultado se sobreescibre, siendo este valor al que las otras funciones acceden para aplicarles el redondeo
+
   document.getElementById("resultado").innerHTML = "= " + resultado;
-  document.getElementById("resultado").style.color = "Black";
+  document.getElementById("resultado").style.color = "white";
 }
+// Funciones de redondeo del resultado (Tomando la variable global ultimoResultado )
+function redondo(resultado) {
+  let redondito = Math.round(resultado);
+  // return Math.round(resultado);
+  return redondito;
+}
+function piso(resultado) {
+  let pisito = Math.floor(resultado);
+  // return  Math.floor(resultado)
+  return pisito;
+}
+function techo(resultado) {
+  let techito = Math.ceil(resultado);
+  // return  Math.ceil(resultado)
+  return techito;
+}
+function opresult(opresult) {
+  if (ultimoResultado === null) return; // Si no se ha hecho ninguna operacion(que no se haya gurdado ningun dato)
 
-// let botonRound = document.getElementById("botonRo");
-// botonRandom.addEventListener("click", redondear() )
+  let resultado;
 
-// let FuncionPiso = document.getElementById("botonPiso");
-// botonRandom.addEventListener("click", piso())
-
-// let Funciontecho = document.getElementById("botontecho");s
-// botonRandom.addEventListener("click", techo());
-
+  if (opresult == "redondo") {
+    resultado = redondo(ultimoResultado); //le envia el valor actual de la variable a la funcion y la regresa con su respectivo cambio
+  } else if (opresult == "piso") {
+    resultado = piso(ultimoResultado);
+  } else if (opresult == "techo") {
+    resultado = techo(ultimoResultado);
+  }
+  document.getElementById("resultado").innerHTML = opresult + " " + "= " +  resultado;
+  document.getElementById("resultado").style.color = "white";
+}
+// Opereaciones para el resultado
+let botonRound = document.getElementById("botonRo");
+botonRound.addEventListener("click", function () {
+  opresult("redondo");
+});
+let FuncionPiso = document.getElementById("botonPiso");
+FuncionPiso.addEventListener("click", function () {
+  opresult("piso");
+});
+let Funciontecho = document.getElementById("botontecho");
+Funciontecho.addEventListener("click", function () {
+  opresult("techo");
+});
 // estas funciones, al presionarlas, se les envia a la funcion regresar, segund la elegida, se ejecuta el bloque de codigo y opertaciones correspondintes
 let botonSuma = document.getElementById("botonSuma");
 botonSuma.addEventListener("click", function () {
@@ -102,9 +133,7 @@ let botonDividir = document.getElementById("botonDivi");
 botonDividir.addEventListener("click", function () {
   regresar("dividir");
 });
-
 /*OPERACIONES EXTRAS*/
-
 let botonPotencia = document.getElementById("botonPot");
 botonPotencia.addEventListener("click", function () {
   regresar("potencia");
@@ -121,11 +150,6 @@ let botonRandom = document.getElementById("botonRand");
 botonRandom.addEventListener("click", function () {
   regresar("randomice");
 });
-
-
-
-/*Finales*/
-
 /**addEventListener es una funcion que se usa para asociar un evento con una accion, como una funcion que se ejecute cuando el evento ocurra
   element.addEventListener("evento", función);
 
@@ -135,19 +159,3 @@ botonRandom.addEventListener("click", function () {
  */
 
 //El producto propios era con numeros del 0 al 9
-
-/*
-  Tambien se pueden cambiar los atributos de una etiqueta por otros,
-  es decir un src, de una imagen al hacer click en ella
-
-  let miImage = document.querySelector("img");
-miImage.onclick = function () {
-  let miSrc = miImage.getAttribute("src");
-  if (miSrc === "images/firefox-icon.png") {
-    miImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    miImage.setAttribute("src", "images/firefox-icon.png");
-  }
-};
-
-  */
