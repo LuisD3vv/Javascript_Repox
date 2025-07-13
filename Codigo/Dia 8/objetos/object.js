459d7e71437b2e1470150bc0b5409a14886eda6a
Lissandro D3v <luismexico2006@gmail.com>
2025-07-12 20:42:53 -0700

    copiando dia 8

---
+++ Codigo/Dia 8/objetos/object.js
@@ -0,0 +1,35 @@
+// Esta es la sintaxis basica de un objeto
+
+let automovil = {
+    marca: "Ford",
+    modelo: "Fiesta",
+    anio: 2015,
+    arrancar() {
+        console.log('En marcha');
+    }
+}
+
+//Podemos llamarlo por cualquiera de sus atributos
+//mediante un punto
+console.log(automovil.anio);
+console.log(automovil.modelo);
+console.log(automovil.marca);
+console.log(automovil.arrancar);
+
+// o tambien
+
+console.log(automovil['anio']);
+console.log(automovil['modelo']);
+console.log(automovil['marca']);
+console.log(automovil['arrancar']);
+
+// Otro ejemplo
+
+let polo = {
+    marca:"Lacoste",
+    color: "Azul",
+    talle: "L",
+    fabricar() {
+        console.log("Polo fabricado");
+    }
+};
